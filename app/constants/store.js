b0205b1578bfdcfe03f300ff59d8869ad40d7974
Danucosuko <71569318+Danucosukosuko@users.noreply.github.com>
2026-02-16 23:46:32 +0100

    Prevent adblock from blocking TuneIn first-party navigation

--- app/constants/store.js
+++ app/constants/store.js
@@ -2,4 +2,8 @@ module.exports = {
   rtid: 'rtid',
   partnerId: 'partnerId',
   windowBounds: 'windowBounds',
+  nuteinTweaksEnabled: 'nuteinTweaksEnabled',
+  nuteinAdsEnabled: 'nuteinAdsEnabled',
+  nuteinDisableTelemetryCrashReports: 'nuteinDisableTelemetryCrashReports',
+  nuteinDisableUpdates: 'nuteinDisableUpdates',
 };
